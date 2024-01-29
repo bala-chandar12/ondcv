b59c4db8b8901e09e1fa44bba48aea10dda15764
Balachandar <105204404+bala-chandar12@users.noreply.github.com>
2024-01-29 19:04:59 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify,request
 from flask_cors import CORS
 from main import predic
+import requests
+import time
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
+def download(url):
+    response = requests.get(url)
+    with open("C:/Users/nagar/PycharmProjects/ondc/content/local_image.jpg", "wb") as f:
+        f.write(response.content)
+
 
 @app.route('/data')
 def get_data():
@@ -13,7 +20,13 @@ def get_data():
 def post_data():
     req_data = request.get_json()
     print(req_data["url"])
-    res=predic(req_data["url"])
+    download(req_data["url"])
+    time.sleep(2)
+
+
+    res=predic("C:/Users/nagar/PycharmProjects/ondc/content/local_image.jpg")
+   
+
 
     # Assuming the request sends a JSON object
     # Do something with the received data
